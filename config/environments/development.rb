a0aee4feaa9ed7d2434ae6727761b5a59c44d90a
Baron-burton <brandon.anthony.burton@gmail.com>
2016-02-04 18:23:54 +0000

    Added Integration Testing and Mailer
    Configured development, test, and production environments. Added a tailored mailer for "order received/shipped" in both plain and HTML. Created functional tests and integration tests.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  
+  # Disables delivery of emails in development
+  # Depot::Application.configure do 
+  #   config.action_mailer.delivery_method = :test
+  # end
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
