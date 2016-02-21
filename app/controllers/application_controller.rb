6a574bb669cf2d460972500f8035a80035609deb
Baron-burton <brandon.anthony.burton@gmail.com>
2016-02-21 12:51:02 +0000

    Adding spanish translation
    Added translation to store front, cart, order page, and certain error/flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,33 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
 	before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   protected
+  # sets the locale from the params but only if there is a locale in the params
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = 
+          "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
 
-  	def authorize
-  		unless User.find_by(id: session[:user_id])
-  			redirect_to login_url, notice: "Please log in"
-  		end
-  	end
+# provides a hash of URL options that are to be considered as present 
+# whenever they aren't otherwise provided
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def authorize
+  	unless User.find_by(id: session[:user_id])
+			redirect_to login_url, notice: "Please log in"
+ 		end
+ 	end
 end
